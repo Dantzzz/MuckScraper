9a1d9ee0d812185f2b39877729a2815abfb58fb7
Dantz Farrow <dantzfarrow@gmail.com>
2021-04-12 00:44:31 -0700

    More test data in progress.

--- MuckScraperTests/UnitTest1.cs
+++ MuckScraperTests/UnitTest1.cs
@@ -18,15 +18,45 @@ namespace MuckScraperTests
     {
         public static void Generate()
         {
-            var ArticlesFaker = new Faker<Article>()
+            int dataCounter = 0;
+
+            var articlesFaker = new Faker<Article>()
                 .RuleFor(articleClass => articleClass.ArticleId, faker => Guid.NewGuid())
                 .RuleFor(articleClass => articleClass.Title, faker => faker.Lorem.Sentence())
+                .RuleFor(articleClass => articleClass.SourceMedium, faker => faker.Lorem.Word())
                 .RuleFor(articleClass => articleClass.AuthorFirstName, faker => faker.Name.FirstName())
-                .RuleFor(articleClass => articleClass.AuthorLastName, faker => faker.Name.LastName());
-                
+                .RuleFor(articleClass => articleClass.AuthorLastName, faker => faker.Name.LastName())
+                .RuleFor(articleClass => articleClass.SourceUrl, faker => faker.Internet.Url())
+                .RuleFor(articleClass => articleClass.PublicationName, faker => faker.Lorem.Word());
+                //.RuleFor(articleClass => articleClass.UploadDate, faker => faker.Date.Past(2, null)
+                //.RuleFor(articleClass => articleClass.PublishDate, faker => faker.Date.Past(2, null);
 
-            var articleGenerated = ArticlesFaker.Generate();
-            Console.WriteLine($"article id : {articleGenerated.ArticleId} firstname: {articleGenerated.AuthorFirstName} lastname: {articleGenerated.AuthorLastName}");
+            var profilesFaker = new Faker<Profile>()
+                .RuleFor(profileClass => profileClass.ProfileId, faker => faker.Random.Number(5000))
+                .RuleFor(profileClass => profileClass.UserId, faker => faker.Random.Number(5000))
+                .RuleFor(profileClass => profileClass.FirstName, faker => faker.Name.FirstName())
+                .RuleFor(profileClass => profileClass.LastName, faker => faker.Name.LastName())
+                .RuleFor(profileClass => profileClass.MiddleName, faker => faker.Name.FirstName());
+                //.RuleFor(profileClass => profileClass.ProfileId, faker => faker.Image.People());
+
+            var usersFaker = new Faker<User>()
+                .RuleFor(userClass => userClass.UserId, faker => faker.Random.Number(5000))
+                .RuleFor(userClass => userClass.Username, faker => faker.Internet.UserName())
+                .RuleFor(userClass => userClass.Password, faker => faker.Internet.Password())
+                //.RuleFor(userClass => userClass.RegistrationDate, faker => faker.Date.Past())
+                //.RuleFor(userClass => userClass.LastLoginDate, faker => faker.Date.Past())
+                .RuleFor(userClass => userClass.Credentials, faker => faker.Random.Byte())
+                .RuleFor(userClass => userClass.PrimaryEmail, faker => faker.Internet.Email());
+
+            while (dataCounter < 51)
+            {
+                var articleGenerated = articlesFaker.Generate();
+                var userGenerated = usersFaker.Generate();
+                var profileGenerated = profilesFaker.Generate();
+                dataCounter++;
+            }
+
+            //Console.WriteLine($"article id : {articleGenerated.ArticleId} firstname: {articleGenerated.AuthorFirstName} lastname: {articleGenerated.AuthorLastName}");
         }
     }
 }
